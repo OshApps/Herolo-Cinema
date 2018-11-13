75e7796bbb8186690b8ff4f9e6f134ea0b2ba59e
Oshri Azarya <oshria6688@gmail.com>
2018-11-13 12:14:50 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from '@app/styles/main.scss';
+
+ReactDOM.render((<h1 className="title">Hello World</h1>), document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+
+module.exports = {
+  mode: 'production',
+  entry: './app/index.js',
+  output: {
+    filename: 'static/js/app.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      "@root": ".",
+      "@app": "app",
+      "@components": "app/components"
+    },
+    modules: [__dirname, 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "static/css/globals.css",
+    }),
+    new CopyWebpackPlugin([
+      { from: 'app/index.html' },
+      // { from: 'app/static/icons' , to: 'static/icons'},
+    ])
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+    stats: "minimal",
+    open: true
+  }
+};
